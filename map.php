bfdc16b6d98bfec722f82733edee4cddcb98c0dc
Ahmed Elgendy <aelgendy@troy.edu>
2018-03-04 19:46:18 -0600

    Added the connect to database file and map file

---
+++ map.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Geolocation</title>
+        <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="style/style.css">
+    </head>
+    <body>
+        <div id="map"></div>
+        <script src="js/plugins.js"></script>
+        <script async defer
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCzMHc5MQBw4UrHMCKTCVmwSngKzo1Kh6I&callback=initMap">
+        </script>
+    </body>
+</html>
