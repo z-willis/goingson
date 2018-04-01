9b5313872e17074afb2aae2c828e998b211dcf41
z-willis <zac.r.willis@gmail.com>
2018-04-01 07:53:44 -0500

    Adds deletion of rows from voting table to allow deletion of events that have been voted on

--- delete_event.php
+++ delete_event.php
@@ -2,6 +2,9 @@
 session_start();
 include "connect.php";
 // Enter the user's information into the database
+$stmt = $con->prepare('DELETE FROM voting WHERE eventid = ?');
+$stmt->execute(array($_POST["eventid"]));
+
 $stmt = $con->prepare('DELETE FROM events WHERE eventid = ?');
 $stmt->execute(array($_POST["eventid"]));
 ?>
